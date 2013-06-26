b8617af59ade56c53d8aa04194dafa58c48d4a64
markdaems <markdaems@d681b350-854c-439c-a81d-c0ce20710203>
2013-06-26 21:07:39 +0000

    7.1.0 Beta version
    git-svn-id: svn://svn.code.sf.net/p/zeoslib/code-0@2439 d681b350-854c-439c-a81d-c0ce20710203

---
+++ tags/7.1.0-beta/packages/lazarus/zplain.pas
@@ -0,0 +1,17 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit zplain;
+
+interface
+
+uses
+  ZPlainDbLibDriver, ZPlainDriver, ZPlainLoader, ZPlainMySqlConstants, 
+  ZPlainMySqlDriver, ZPlainOracleConstants, ZPlainOracleDriver, 
+  ZPlainPostgreSqlDriver, ZPlainSqLiteDriver, ZPlainASADriver, ZPlainASAConstants,
+  ZPlainFirebirdDriver, ZPlainFirebirdInterbaseConstants, ZPlainDbLibConstants;
+
+implementation
+
+end.
